e08dff03ff5db721059fd2bf6e9b0232281a39b8
Janith Sandamal <78975250+Janith-Sandamal@users.noreply.github.com>
2023-07-17 23:10:19 +0530

    Header component finished ✅

--- client/src/pages/home/Home.js
+++ client/src/pages/home/Home.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import Header from '../../components/header/Header'
 
 const Home = () => {
   return (
     <div>
+        <Header/>
         <h1>Home</h1>
     </div>
   )
