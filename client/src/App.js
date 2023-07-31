4a3778498f0a09c1ddba074738cc2b000951a5d6
Janith Sandamal <78975250+Janith-Sandamal@users.noreply.github.com>
2023-07-31 21:35:00 +0530

    setup card component

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Home from "./pages/home/Home";
 import Layout from "./components/layout/Layout";
+import Login from "./pages/auth/Login";
 
 
 function App() {
@@ -8,7 +9,8 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Layout> <Home /> </Layout>} />
+          <Route path="/" element={<Layout> <Home /> </Layout>} /> {/* Route for Home page */}
+          <Route path="/login" element={ <Login /> } />             {/* Route for Login page */}
         </Routes>
       </BrowserRouter>
     </>
